f9c0af08afaa517649b0c8f67b383b8693ed990e
Steven E. Gloyd <jemimacakes@gmail.com>
2015-08-20 00:26:18 -0700

    Initial Commit
    Compiles, but having trouble when actually creating an object.

---
+++ PSRCAN.cpp
@@ -0,0 +1,85 @@
+#include "PSRCAN.h"
+
+PSRCAN::PSRCAN(byte _CS) : mcpCAN(_CS) {
+	mcpCAN.begin(9);
+
+	for(int i = 0; i < 8; i++)
+		buf[i] = 0;
+
+	bufLen = 0;
+}
+
+void PSRCAN::addData(int loc, bool data){
+	byte byteBool;
+
+	if(data)
+		byteBool = 0xFF;
+	else
+		byteBool = 0x00;
+
+	buf[loc] = byteBool;
+
+	bufLen += 1;
+
+}
+
+void PSRCAN::addData(int loc, char data){
+	buf[loc] = data;
+
+	bufLen += 1;
+}
+
+void PSRCAN::addData(int loc, byte data){
+	buf[loc] = data;
+
+	bufLen += 1;
+};
+
+void PSRCAN::addData(int loc, int data){
+	for(int i = 0; i < 1; i++){
+		buf[loc + i] = ((data >> (8 * (1 - i))) & 0xFF);
+	}
+
+	bufLen += 2;
+}
+
+void PSRCAN::addData(int loc, unsigned int data){
+	for(int i = 0; i < 1; i++){
+		buf[loc + i] = ((data >> (8 * (1 - i))) & 0xFF);
+	}
+
+	bufLen += 2;
+}
+
+void PSRCAN::addData(int loc, long data){
+	for(int i = 0; i < 4; i++){
+		buf[loc + i] = ((data >> (8 * (3 - i))) & 0xFF);
+	}
+
+	bufLen += 4;
+}
+
+void PSRCAN::addData(int loc, unsigned long data){
+	for(int i = 0; i < 4; i++){
+		buf[loc + i] = ((data >> (8 * (3 - i))) & 0xFF);
+	}
+
+	bufLen += 4;
+}
+
+void PSRCAN::addData(int loc, float data){
+	for(int i = 0; i < 1; i++){
+		buf[loc + i] = ((*reinterpret_cast<int*>(&data) >> (8 * (3 - i))) & 0xFF);
+	}
+
+	bufLen += 4;
+}
+
+void PSRCAN::sendMSG(unsigned long ID, byte funcNum){
+	mcpCAN.sendMsgBuf(ID, funcNum, bufLen, buf);
+
+	for(int i = 0; i < 8; i++)
+		buf[i] = 0;
+
+	bufLen = 0;
+}
---
+++ PSRCAN.h
@@ -0,0 +1,25 @@
+#ifndef _PSRCAN
+#define _PSRCAN
+
+#include "../MCP_CAN_lib-master/mcp_can.h"
+
+class PSRCAN
+{
+	private:
+		MCP_CAN mcpCAN;
+		INT8U buf[8];
+		int bufLen;
+	public:
+		PSRCAN(byte _CS);
+		void addData(int loc, bool data);			//add 1 byte
+		void addData(int loc, char data);			//add 1 byte
+		void addData(int loc, byte data);			//add 1 byte
+		void addData(int loc, int data);			//add 2 bytes
+		void addData(int loc, unsigned int data);	//add 2 bytes
+		void addData(int loc, long data);			//add 4 bytes
+		void addData(int loc, unsigned long data);	//add 4 bytes
+		void addData(int loc, float data);			//add 4 bytes
+		void sendMSG(unsigned long ID, byte funcNum);
+};
+
+#endif
